6271f1f02937e66523d26e203eb269ad01b385a1
MRenteriaDev <mrenteriadev@outlook.com>
2022-12-22 17:04:50 -0600

    section 8 ready

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < dateOfBirth.DayOfYear)
+                age = age - 1;
+
+            return age;
+        }
+    }
+}

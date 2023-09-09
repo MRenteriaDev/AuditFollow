6be8e36a9835776c0e983ee7e299da3f3720df0c
Moisés Rentería <mrenteriadev@outlook.com>
2023-09-08 19:06:53 -0700

    proceso de alta en el signalR

---
+++ API/Entities/Connection.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Entities;
+
+public class Connection
+{
+    public Connection()
+    {
+        
+    }
+    public Connection(string connectionid, string username)
+    {
+        ConnectionId = connectionid;
+        Username = username;
+    }
+
+    public string ConnectionId { get; set; }
+    public string Username { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
+using API.SignalR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -25,7 +26,10 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 //Especific cors
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
     .WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
@@ -33,6 +37,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<PressenceHub>("hubs/pressence");
+app.MapHub<MessageHub>("hubs/message");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
